198325208f3f6bf94bda2a3b709442c3892a2891
MarcosViniSil <Marcossilv203@gmail.com>
2024-05-01 14:19:17 -0300

    feat: cria conexão com o discord e envia uma mensagem

--- Program.cs
+++ Program.cs
@@ -1,13 +1,36 @@
-﻿
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
-namespace Bot_Translate
-{
-    internal class Program
-    {
-        static void Main(string[] args)
+
+        async Task RunBotAsync()
         {
             connectionDiscord connection = new connectionDiscord();
-            Console.WriteLine(connection.getToken());
+            
+            var client = new DiscordSocketClient();
+            string token = connection.getToken();
+            Console.WriteLine(token);
+            await client.LoginAsync(TokenType.Bot, token);
+            
+            Console.WriteLine(client.LoginState);
+            
+            await client.StartAsync();
+
+            client.Ready += async () =>
+            {
+                var guild = client.GetGuild(1235242325500887050);
+                var channel = guild.GetTextChannel(1235242325500887053);
+    
+                await channel.SendMessageAsync("Hello World");
+    
+                await client.DisposeAsync();
+            };
+
+
         }
-    }
-}
+
+        await RunBotAsync();
+        Console.ReadKey();
+    
+
